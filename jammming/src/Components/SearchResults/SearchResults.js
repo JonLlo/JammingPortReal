7c2b36514158843833e0247c3d37b2082780f1f5
Jonny Lloyd <154521758+JonLlo@users.noreply.github.com>
2024-01-25 18:08:46 +0000

    up to 18

---
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './SearchResults.css';
+
+function SearchResuts() {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      //Add a TrackList component
+    
+    </div>
+  );
+}
+
+export default SearchResults;
